8640d9aea1b03bcfec7ed58a11fa789a16550218
Max-Bld <mblanchard95@gmail.com>
2024-02-19 16:38:17 +0100

    sparql queries: Hotel, Restaurant and PointOfInterest

--- sparql_query.py
+++ sparql_query.py
@@ -7,11 +7,12 @@ Created on Mon Feb 19 11:51:30 2024
 """
 
 from rdflib import Graph
-from pprint import pprint
 
 g = Graph()
 
 g.parse("data/flux-19287-202401180748.ttl")
+
+#%% Query all distinct classes
 
 q = """
     SELECT DISTINCT ?classes
@@ -22,6 +23,47 @@ q = """
 
 q_response = g.query(q)
 
-#%%
 for row in q_response:
     print(f"{row.classes}")
+    
+#%% Query all Hotel entities
+
+q = """
+    SELECT DISTINCT ?subject
+    WHERE {
+        ?subject rdf:type <https://www.datatourisme.fr/ontology/core#Hotel> .
+    }
+"""
+
+q_response = g.query(q)
+
+for row in q_response:
+    print(f"{row.subject}")
+    
+#%% Query all Restaurant entities
+
+q = """
+    SELECT DISTINCT ?subject
+    WHERE {
+        ?subject rdf:type <https://www.datatourisme.fr/ontology/core#Restaurant> .
+    }
+"""
+
+q_response = g.query(q)
+
+for row in q_response:
+    print(f"{row.subject}")
+    
+#%% Query all PointOfInterest entities
+
+q = """
+    SELECT DISTINCT ?subject
+    WHERE {
+        ?subject rdf:type <https://www.datatourisme.fr/ontology/core#PointOfInterest> .
+    }
+"""
+
+q_response = g.query(q)
+
+for row in q_response:
+    print(f"{row.subject}")
