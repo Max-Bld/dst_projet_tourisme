d12e5fa26d00ce23e170fd5e6b8c2f32389f567d
alexandretitus <49631891+alexandretitus@users.noreply.github.com>
2024-02-22 12:43:32 +0000

    modification 22022024 16:50

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from extract_data import data
-from cli import cli_test
-from visualisation_data import visualize_data
+from libraries.extract_data import data
+from libraries.cli import cli_test
+from libraries.visualisation_data import visualize_data
 
 
 latitude_user, longitude_user, perimetre_user = cli_test(data)
 m = visualize_data(data, latitude_user, longitude_user, perimetre_user)
-m.save("test_print8.html")
+m.save("display/test_print9.html")
 
