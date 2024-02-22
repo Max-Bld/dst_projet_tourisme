8a917979909693437d746ddd3a475c948de4cabd
alexandretitus <49631891+alexandretitus@users.noreply.github.com>
2024-02-22 12:39:17 +0000

    modification 22022024 16:39 REUNION

---
+++ libraries/extract_data.py
@@ -0,0 +1,22 @@
+import rdflib
+from rdflib import Graph
+
+# Charger les données TTL dans un graph RDF
+g = Graph()
+g.parse("data/flux-19287-202401180748.ttl")
+
+# Requête SPARQL pour extraire les informations sur la localisation
+q = """
+    SELECT ?name ?lat ?lon  
+    WHERE {
+        ?restaurant rdf:type core:Restaurant.
+        ?restaurant rdfs:label ?name.
+        ?restaurant core:isLocatedAt ?localisationuri.
+        ?localisationuri schema1:geo ?geouri.
+        ?geouri schema1:latitude ?lat ; schema1:longitude ?lon.
+        
+    }
+"""
+
+data = g.query(q)
+
