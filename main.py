37124a3a86dcecff5ae5e38d239c2fb059b0698b
wissemej <147838884+wissemej@users.noreply.github.com>
2024-02-22 14:36:28 +0000

    modification

--- main.py
+++ main.py
@@ -6,4 +6,3 @@ from libraries.visualisation_data import visualize_data
 latitude_user, longitude_user, perimetre_user = cli_test(data)
 m = visualize_data(data, latitude_user, longitude_user, perimetre_user)
 m.save("display/test_print9.html")
-
