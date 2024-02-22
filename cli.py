d843d8b02bd7ba47c2657f96265c5755d355400f
Max-Bld <mblanchard95@gmail.com>
2024-02-22 18:43:25 +0100

    template utilisable, manque plus que l'affichage sur une carte

---
+++ cli.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 11:29:43 2024
+
+@author: maxbld
+
+Affiche des informations à l'utilisateur.
+
+Permet à l'utilsateur d'entrer des préférences qui modifieront
+la requête SPARQL.
+
+"""
+
+from sys import exit
+from time import sleep
+from display import visualize_data
+
+def se_geolocaliser(latitude_user, longitude_user):
+    
+        # Géolocalisation de l'utilisateur
+    
+    entree=input("""Choisissez le mode de géolocalisation :
+        
+        (a) manuel
+        (b) aléatoire
+    """)
+    
+    if entree=="a": # manual
+        try: 
+            latitude_user = float(input("Choisissez une latitude : "))
+            longitude_user = float(input("Choisissez une longitude : "))
+        except:
+            print("Mauvaise entrée.")
+            exit()
+            
+    elif entree=="b": #random
+    
+        print(f"Ma géolocalisation générée aléatoirement dans le département de la Meuse : \n{latitude_user}, {longitude_user}")
+    
+    else:
+        
+        print("Mauvaise entrée.")
+        exit()
+
+def choisir_perimetre(perimetre_user):
+    
+    entree=input("""\nChoisissez le périmètre :
+        
+        (a) manuel
+        (b) aléatoire
+    """)
+    
+    if entree=="a":
+    
+        try:
+            perimetre_user = int(input("\nPérimètre de déplacement : "))
+        except:
+            print("Mauvaise entrée.")
+            exit()
+    
+    elif entree=="b":
+        
+        print(f"\nMon périmètre de déplacement généré aléatoirement : {perimetre_user}")
+    
+    else:
+        
+        print("Mauvaise entrée.")
+        exit()
+    
+def choisir_preferences():
+        
+    entree=input("""\nQue cherchez vous :
+        (a) Restaurant
+        (b) Hôtel
+        (c) Point d'intérêt touristique
+          """)
+    
+    if entree=="a":
+        query_element="Restaurant"
+    elif entree=="b":
+        query_element="Hotel"
+    elif entree=="c":
+        query_element="PointOfInterest"
+    else:
+        print("Mauvaise entrée.")
+        exit()
+        
+    return query_element
+
+def retourner_lieux(queried_data):
+    input("\nAppuyez sur une touche pour afficher le nom des lieux et leur géolocalisation :")
+    for row in queried_data:
+        print(f"{row.name} - {row.lat},{row.lon}")
+        sleep(0.01)
+        
+def retourner_distances(queried_data, euclidean_distances):
+    i=0
+    #input("\nAppuyez sur une touche pour afficher le noms des lieux et leur distance :")
+    for row in queried_data:
+        print(f"{row.name} - ", euclidean_distances[i])
+        i=i+1
+        sleep(0.01)
+        
+def afficher_carte(queried_data, latitude_user, longitude_user, perimetre_user):
+    input("\nAppuyez sur une touche pour afficher la carte :")
+    visualize_data(queried_data, latitude_user, longitude_user, perimetre_user)
---
+++ display.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 11:30:12 2024
+
+@author: maxbld
+
+Affiche sur une carte les données de géolocalisation de 
+l'utilisateur et des établissements.
+
+"""
+
+import rdflib
+from rdflib import Graph
+import folium
+import random
+import geopy.distance
+from pyroutelib3 import Router
+
+def visualize_data(data, latitude_user, longitude_user, perimetre_user):
+
+    coords_user = (latitude_user, longitude_user)
+
+    router = Router("car")
+
+    m = folium.Map([latitude_user, longitude_user])
+
+    depart = router.findNode(latitude_user, longitude_user)
+
+
+    # Afficher les résultats
+    for row in data:
+        name = row.name
+        lat = float(row.lat)
+        lon = float(row.lon)
+
+
+        coords_resto = (lat, lon)
+        distance = int(geopy.distance.geodesic(coords_user, coords_resto).km)
+
+        popup_description = name + "\n\n" + "distance en km :" + str(distance)
+
+        folium.Marker(
+            location=[lat, lon],
+            tooltip=popup_description,
+            popup=popup_description,
+            icon=folium.Icon(color="blue"),
+        ).add_to(m)
+
+        arrivee = router.findNode(lat, lon)
+        status, itineraire = router.doRoute(depart, arrivee)
+
+        if status == 'success':
+            routeLatLons = list(map(router.nodeLatLon, itineraire)) # liste des points du parcours
+        
+        carte= folium.Map(location=[(latitude_user+lat)/2,(longitude_user+lon)/2],zoom_start=15)
+
+        itineraire_coordonnees = list(map(router.nodeLatLon, itineraire)) # liste des points du parcours
+
+        folium.PolyLine(
+            itineraire_coordonnees,
+            color="blue",
+            weight=2.5,
+            opacity=1
+            ).add_to(m)
+
+
+    #le radius est le rayon en metres
+    folium.Circle(
+        location=[latitude_user, longitude_user],
+        radius=perimetre_user,
+        color="black",
+        weight=1,
+        fill_opacity=0.6,
+        opacity=1,
+        fill_color="green",
+        fill=False,  # gets overridden by fill_color
+        popup="{} meters".format(perimetre_user),
+        tooltip="I am in meters",
+    ).add_to(m)
+
+    folium.Marker(
+        location=[latitude_user, longitude_user],
+        tooltip="Votre position",
+        popup="Votre position",
+        icon=folium.Icon(color="red"),
+    ).add_to(m)
+
+    return m
