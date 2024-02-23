6e4ca85c33d0f1e92f3047ac87dccabbf6e5c8a7
alexandretitus <49631891+alexandretitus@users.noreply.github.com>
2024-02-23 17:19:24 +0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from libraries.extract_data import data
-from libraries.cli import cli_test
-from libraries.visualisation_data import visualize_data
-
-
-latitude_user, longitude_user, perimetre_user = cli_test(data)
-m = visualize_data(data, latitude_user, longitude_user, perimetre_user)
-m.save("display/test_print9.html")
-
